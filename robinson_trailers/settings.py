9221fd35cf7173b01ce70d4cb1914f4f94e8e89e
FabrizioAdi <adriannoszczyk@gmail.com>
2022-06-10 10:51:36 +0000

    Update allowed host for heroku

--- robinson_trailers/settings.py
+++ robinson_trailers/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["robinson-trailers-store.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["robinson-trailers-store.herokuapp.com", "localhost"]
 
 
 # Application definition
