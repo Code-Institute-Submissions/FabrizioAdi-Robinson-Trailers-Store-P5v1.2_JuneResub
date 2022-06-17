fa1125b043e81988ab1267479085f86c4caee7ec
FabrizioAdi <adriannoszczyk@gmail.com>
2022-06-17 11:16:04 +0000

    Add update total method, calculate for delivery cost

--- checkout/models.py
+++ checkout/models.py
@@ -26,9 +26,18 @@ class Order(models.Model):
         """ Generate a random, unique order namber using uuid. """
         return uuid.uuid4().hex.upper()
 
+    def update_total(self):
+        """ Update grand total each time a line item is added, accounting for delivery costs. """
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
+            self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        else:
+            self.delivery_cost = 0
+        self.grand_total = self.order_total + self.delivery_cost
+        self.save()
+
     def save(self, *args, **kwargs):
-        """ Override the original save method to set the order number
-        if it hasn't been set already. """
+        """ Override the original save method to set the order number if it hasn't been set already. """
         if not self.order_number:
             self.order_number = self._generate_order_number()
         super().save(*args, **kwargs)
