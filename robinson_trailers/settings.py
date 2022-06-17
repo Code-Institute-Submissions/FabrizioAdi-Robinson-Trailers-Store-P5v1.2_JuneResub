f980fe1914bbc95e6add0ac1b22c99b513f2498b
FabrizioAdi <adriannoszczyk@gmail.com>
2022-06-17 12:36:02 +0000

    Crispy forms installation

--- robinson_trailers/settings.py
+++ robinson_trailers/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'products',
     'cart',
     'checkout',
+    # Other app
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +66,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'robinson_trailers.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -82,6 +86,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
